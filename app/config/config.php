21dc647a1417aa210e6ed795b1f5e7ce976ea6e3
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-25 09:02:45 -0600

    cleaned some things up

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,13 @@
 <?php
   // DB Params
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', '9bDDC7_XGp&Pv$H');
-  define('DB_NAME', 'gmvc');
+  define('DB_USER', '_YOUR_USER_');
+  define('DB_PASS', '_YOUR_PASS_');
+  define('DB_NAME', '_YOUR_DBNAME_');
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/gumenymvc');
+  define('URLROOT', '_YOUR_URL_');
   // Site name
-  define('SITENAME', 'GumenyMVC');
+  define('SITENAME', '_YOUR_SITENAME_');
 ?>
