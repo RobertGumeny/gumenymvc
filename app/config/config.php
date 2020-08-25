5468eece574fa5437014a31c03d2e6ad9e08919c
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-24 22:55:17 -0600

    config and library autoloader

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php 
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/gumenymvc');
+  // Site name
+  define('SITENAME', 'GumenyMVC');
+?>
