e0af68de2c1a4af02e4f2b92095aa1c5afc8b7f2
Robert Gumeny <robert.gumeny528@gmail.com>
2020-08-24 23:48:18 -0600

    connecting to DB

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '9bDDC7_XGp&Pv$H');
+  define('DB_NAME', 'gmvc');
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
